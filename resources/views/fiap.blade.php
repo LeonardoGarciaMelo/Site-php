8c2dfc343ae29b92cf55ba68bf3792d3570d4df2
Leonardo Garcia Melo <lgarciamelo213@gmail.com>
2025-01-09 12:36:09 -0300

    First commit

---
+++ resources/views/fiap.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site da Faculdade</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div>classic {
+
+position: absolute;
+
+top: 50%;
+
+left: 50%;
+
+transform: translate(-50%, -50%);
+
+}
+        <h1>Bem-vindo ao site da faculdade</h1>
+        <p>Esta é a minha página inicial personalizada.</p>
+    </div>
+</body>
+</html>
